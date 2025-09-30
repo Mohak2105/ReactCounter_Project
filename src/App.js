697fd39243a62ff59d754a642f0525e21c6a4ce3
Mohak <mohaksidgo603@gmail.com>
2025-09-30 19:29:14 +0530

    Modal activated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from './components/Counter';
 function App() {
   return (
    <>
+   {/* <Counter/> */}
    <Counter/>
     </>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,46 +1,66 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-    const [count, setCount] = useState(0);
+  const Increament = () => setCount(count + 1);
+  const Decrement = () => setCount(count - 1);
 
-    const Increament = () => {
-        setCount(count + 1)
-    }
+  const Reset = () => {
+    setShowModal(true); 
+  };
 
-    const Decrement = () => {
-        setCount(count - 1)
-    }
+  const confirmReset = () => {
+    setCount(0);
+    setShowModal(false); 
+  };
 
-    const Reset = () => {
-        setCount(0)
-    }
+  const cancelReset = () => {
+    setShowModal(false);
+  };
 
-    return (
-        <>
-            <div className='Main'>
-                <div className='Row'>
-                    <li className='Dec'><span className="material-symbols-outlined" onClick={Decrement}>do_not_disturb_on</span></li>
-                    <h1 className='Count'>{count}</h1>
-                    <li className='Inc'><span className="material-symbols-outlined" onClick={Increament}>add_circle</span></li>
-                </div>
+  return (
+    <>
+      <div className="Main">
+        <div className="Row">
+          <li className="Dec">
+            <span className="material-symbols-outlined" onClick={Decrement}>
+              do_not_disturb_on
+            </span>
+          </li>
+          <h1 className="Count">{count}</h1>
+          <li className="Inc">
+            <span className="material-symbols-outlined" onClick={Increament}>
+              add_circle
+            </span>
+          </li>
+        </div>
 
-                <li className='Res'><span className="material-symbols-outlined" onClick={Reset}>refresh</span></li>
+        <li className="Res">
+          <span className="material-symbols-outlined" onClick={Reset}>
+            refresh
+          </span>
+        </li>
 
-                <div  className="modal">
-                   
-                    <div className="modal-content">
-                        <span class="material-symbols-outlined close">cancel</span>
-                        <div className='message'>
-                            <p className='reset'>Reset?</p>
-                            <button className='button'>Yes</button>
-                            <button className='button'>Cancel</button>
-                        </div>
-                    </div>
-                </div>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <div className="message">
+                <p className="reset">Reset?</p>
+                <button className="button" onClick={confirmReset}>
+                  Yes
+                </button>
+                <button className="button" onClick={cancelReset}>
+                  Cancel
+                </button>
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Counter
+export default Counter;
